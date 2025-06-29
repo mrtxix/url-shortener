cd5c7d471822df986ac4d4a07e1ca6b3019bec89
shubhamv1111 <svsv26551@gmail.com>
2025-06-29 23:55:16 +0530

    add mongoose connection and database models

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URL || "mongodb://localhost:27017/urlshortener", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 // Basic middleware
 app.use(express.urlencoded({ extended: false }));
